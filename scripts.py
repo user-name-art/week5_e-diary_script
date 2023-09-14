389d2addf02188fc247afab7e83a01caaecc26a8
Art Gilyazov <art.gilyazov@mail.ru>
2023-09-14 19:58:07 +0300

    used model exceptions instead of general ones

--- scripts.py
+++ scripts.py
@@ -1,5 +1,4 @@
 import random
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 
 
@@ -18,9 +17,9 @@ COMPLIMENTS = [
 def get_schoolkid_by_name(schoolkid_name):
     try:
         return Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except ObjectDoesNotExist:
+    except Schoolkid.DoesNotExist:
         print('Ученика с таким именем нет в базе данных.')
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print('Учеников с таким именем несколько.')
 
 
